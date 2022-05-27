501174d0620d92726f62cdaf2ec8f35c2f62061b
sophiecode1105 <sophiecode1105@gmail.com>
2022-05-27 17:04:52 +0900

    add: 기본장면만들기camera

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { Scene } from 'three';
 
 //동적으로 캔버스 조립하기
 // const renderer = new THREE.WebGL1Renderer();
@@ -11,3 +12,16 @@ import * as THREE from 'three';
 const canvas = document.getElementById('#three-canvas');
 const renderer = new THREE.WebGL1Renderer({ canvas }); //canvas라는 값을 canvas로 가져오는것
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+//Scene
+
+const scene = new THREE.Scene();
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75, //시야각 field of view
+  window.innerWidth / window.innerHeight, //aspec
+  0.1, // near
+  1000 //far
+);
+scene.add(carmera);
