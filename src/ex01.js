44f698a439cc2a48f099a705fe6730b04df476e7
sophie <sophiecode1105@gmail.com>
2022-08-17 18:57:09 +0900

    update: 개발을 편리하게 해주는 도구들

--- src/ex01.js
+++ src/ex01.js
@@ -1,68 +1,60 @@
 import * as THREE from 'three';
 
-//동적으로 캔버스 조립하기
-// const renderer = new THREE.WebGL1Renderer();
-// //브라우저창사이즈로 사이즈를 지정해준다.
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// //렌더러가 생성이된다면 렌더러가 가진 dom엘리먼트가있다. 렌더러가 가진 캔버스다.
-// console.log(renderer.domElement);
-// document.body.appendChild(renderer.domElement);
+// ----- 주제: AxesHelper, GridHelper
 
-const example = () => {
+export default function example() {
+  // Renderer
   const canvas = document.querySelector('#three-canvas');
-  const renderer = new THREE.WebGL1Renderer({ canvas, antialias: true }); //canvas라는 값을 canvas로 가져오는것, antialias 성능저하 떨어지고 부드럽게해줌
-  renderer.setSize(window.innerWidth, window.innerHeight); //브라우저 창크기로 맞춘것
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-  //Scene
-
+  // Scene
   const scene = new THREE.Scene();
 
-  //Camera
-  // const camera = new THREE.PerspectiveCamera(
-  //   75, //시야각 field of view
-  //   window.innerWidth / window.innerHeight, //aspec 종횡비
-  //   0.1, // near
-  //   1000 //far
-  // );
-  // //카메라와 object가 같은위치면 보이지않으니 뒤로 약간 빼주는 작업을 해야한다.
-  // camera.position.x = 1;
-  // camera.position.y = 2;
-  // camera.position.z = 5; //5정도 거리로 뒤로 빼주는것 + 방향으로
-  // scene.add(camera);
-
-  //Orthgraphic Camera(직교 카메라)
-  const camera = new THREE.OrthographicCamera(
-    -(window.innerWidth / window.innerHeight), //left
-    window.innerWidth / window.innerHeight, //right
-    1, //top
-    -1, //bottom
-    0.1, //near
-    1000 //far
-  );
-  camera.position.x = 1;
-  camera.position.y = 2;
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 5;
-  camera.lookAt(0, 0, 0); //원점을 바라보게
-  camera.zoom = 0.5; //각도 바꾸려면 z건드는것이 아니고 zoom
-  camera.updateProjectionMatrix();
   scene.add(camera);
 
-  //mesh
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.z = 2;
+  scene.add(light);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1); //직육면체  geometry 박스를 하나만든다
-  const material = new THREE.MeshBasicMaterial({
-    // color: oxff0000
-    // color: '#ff0000'
-    color: 'red',
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: 'seagreen',
   });
-
   const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh); //씬에 올려주기
+  scene.add(mesh);
 
-  //그리기
-  //렌더러가 렌더를 해줘야지 우리눈에보인다.
+  // 그리기
+  const clock = new THREE.Clock();
 
-  renderer.render(scene, camera);
-};
+  function draw() {
+    const time = clock.getElapsedTime();
 
-export default example;
+    mesh.rotation.y = time;
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
